9e86784bafa6a7bf8eef78a9279aba589fd90674
will <will@willlam.net>
2016-01-21 12:00:18 -0500

    first commit for phonegotchi assignment

---
+++ FastestRPM/FastestRPM/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FastestRPM
+//
+//  Created by William Lam on 2016-01-21.
+//  Copyright © 2016 William Lam. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ FastestRPM/FastestRPM/ViewController.m
@@ -0,0 +1,30 @@
+//
+//  ViewController.m
+//  FastestRPM
+//
+//  Created by William Lam on 2016-01-21.
+//  Copyright © 2016 William Lam. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+@property (weak, nonatomic) IBOutlet UIImageView *needle;
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+	[super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+	[super didReceiveMemoryWarning];
+	// Dispose of any resources that can be recreated.
+}
+- (IBAction)onDragGesture:(UIPanGestureRecognizer *)sender {
+–––}
+
+@end
