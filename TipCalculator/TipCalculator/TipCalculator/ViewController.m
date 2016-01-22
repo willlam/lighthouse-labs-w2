7f0a7b912551f6bf2e303c14ff91f137327d258a
will <will@willlam.net>
2016-01-22 13:31:40 -0500

    added 15% tip button and displayed label

--- TipCalculator/TipCalculator/TipCalculator/ViewController.m
+++ TipCalculator/TipCalculator/TipCalculator/ViewController.m
@@ -10,7 +10,13 @@
 
 @interface ViewController ()
 
+
+@property (strong, nonatomic) IBOutlet UITextField *billAmountTextField;
+@property (strong, nonatomic) IBOutlet UIButton *calculateButton;
+@property (weak, nonatomic) IBOutlet UILabel *finalAmountLabel;
+
 @end
+
 
 @implementation ViewController
 
@@ -23,5 +29,30 @@
 	[super didReceiveMemoryWarning];
 	// Dispose of any resources that can be recreated.
 }
+- (IBAction)calculateTip:(UIButton *)sender {
+	
+//	if (self.billAmountTextField = nil) && self.billAmountTextField )
+//	[self.billAmountTextField becomeFirstResponder];
+
+
+	
+	NSLog(@"Calculate Tip Button works");
+	
+	// configure variables to do math
+	
+	float billAmount = [self.billAmountTextField.text floatValue];
+	
+	float tip15 = 1.15;
+	
+	
+	// the math
+	
+	float totalPlus15 = billAmount * tip15;
+	
+	// update labels in the view
+	
+	self.finalAmountLabel.text = [NSString stringWithFormat:@"$%.2f", totalPlus15];
+	self.finalAmountLabel.hidden = NO;
+	}
 
 @end
