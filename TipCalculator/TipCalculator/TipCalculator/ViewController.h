3907c9544034713acc0405f0b34dcfc204ec24ec
will <will@willlam.net>
2016-01-22 11:09:14 -0500

    first commit

---
+++ TipCalculator/TipCalculator/TipCalculator/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TipCalculator
+//
+//  Created by William Lam on 2016-01-22.
+//  Copyright © 2016 William Lam. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
